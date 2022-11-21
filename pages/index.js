63edb5ee08702fa4e8e56b82229f7cbf67783942
plastikmind <mert.topkaya1@gmail.com>
2022-11-21 21:20:02 +0300

    title update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ProductScroll from "../components/ProductScroll";
@@ -8,6 +8,9 @@ import Hero from "../components/Hero";
 export default function Alter() {
   return (
     <div className="">
+      <Head>
+        <title>Köksal Atay</title>
+      </Head>
       <Header />
 
       <Hero />
